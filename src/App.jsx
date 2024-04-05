cacdff95c28ee6b2ffec2ebfd2b606f261a8d704
andre.silva <andre.silva@marimex.com.br>
2024-04-05 09:49:04 -0300

    Criação e Estilização da Lista de Artigos, Trabalhando com Imagens e Listando Tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import ArticleList from "./components/ArticleList/ArticleList"
 import Header from "./components/Header/Header"
 
 const App = () => {
   return (
-    <Header />
+      <>
+      <Header />
+      <ArticleList/>
+      </>
   ) 
 }
 
